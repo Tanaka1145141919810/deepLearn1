0d288e0895828130bcddef6a65d1b9603b9136ad
lili <xdu2814031084@1163.com>
2024-09-29 23:08:54 +0800

    add

--- mypytorch/Unet/__init__.py
+++ mypytorch/Unet/__init__.py
@@ -5,4 +5,8 @@ ToDO
 from .modelNet import DoubleConv
 from .modelNet import Down
 from .modelNet import UNet
+from .dice_coeffient import build_target
+from .dice_coeffient import multiclass_dice_coeff
+
+
 __all__ = ["modelNet"]
